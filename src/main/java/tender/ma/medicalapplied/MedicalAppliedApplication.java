aedfea58244d8146ebbc4e09e0d0850a2b698e11
Рушан Рамазанов <rrramazanov@lanit.ru>
2026-04-21 18:29:41 +0400

    add start application

---
+++ src/main/java/tender/ma/medicalapplied/MedicalAppliedApplication.java
@@ -0,0 +1,20 @@
+package tender.ma.medicalapplied;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import tender.ma.medicalapplied.config.ai.DeepseekAiConfig;
+
+
+@EnableScheduling
+@EnableJpaAuditing
+@SpringBootApplication
+@EnableConfigurationProperties({DeepseekAiConfig.class})
+public class MedicalAppliedApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalAppliedApplication.class, args);
+	}
+
+}
